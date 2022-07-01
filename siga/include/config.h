cdd82a0cb2f040dec21fe055a8a6ec8bdb4699ee
Maria Eduarda Ribeiro Facio <84473064+dudaribeiro7@users.noreply.github.com>
2022-07-01 19:15:01 -0300

    Add files via upload

---
+++ siga/include/config.h
@@ -0,0 +1,6 @@
+/* #undef CLASSROOM */
+#define CPU_TIME
+
+#define INPUT_DIR "/home/dudaribeirof/dcc012b-lab05-dudaribeiro7/input/"
+#define EXPECTED_OUTPUT_DIR "/home/dudaribeirof/dcc012b-lab05-dudaribeiro7/tests/expected_output/"
+
